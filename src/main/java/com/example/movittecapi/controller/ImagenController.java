17a46b15e24707912446c4cbd1c2493995164756
Sebastian <spriggangenesiswhip12@gmail.com>
2026-08-02 15:51:27 -0600

    controller de imagen actualizado

--- src/main/java/com/example/movittecapi/controller/ImagenController.java
+++ src/main/java/com/example/movittecapi/controller/ImagenController.java
@@ -1,5 +1,8 @@
 package com.example.movittecapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,8 +15,9 @@ import java.nio.file.Paths;
 @RequestMapping("/imagenes")
 public class ImagenController {
 
-
-    private final String carpeta = "src/main/resources/static/imagenes/";
+    // Carpeta REAL en Render
+    private final String carpeta =
+            System.getProperty("user.dir") + "/imagenes/";
 
 
     @PostMapping("/subir")
@@ -27,25 +31,45 @@ public class ImagenController {
                 directorio.mkdirs();
             }
 
-
             Path ruta = Paths.get(
                     carpeta + archivo.getOriginalFilename()
             );
-
 
             Files.write(
                     ruta,
                     archivo.getBytes()
             );
 
-
             return archivo.getOriginalFilename();
-
 
         } catch (Exception e) {
 
             return "ERROR: " + e.getMessage();
+        }
+    }
 
+
+    @GetMapping("/{nombre}")
+    public ResponseEntity<Resource> obtenerImagen(
+            @PathVariable String nombre
+    ) {
+
+        try {
+
+            Path ruta = Paths.get(carpeta + nombre);
+
+            Resource recurso = new UrlResource(ruta.toUri());
+
+            if (recurso.exists()) {
+
+                return ResponseEntity.ok(recurso);
+            }
+
+            return ResponseEntity.notFound().build();
+
+        } catch (Exception e) {
+
+            return ResponseEntity.notFound().build();
         }
     }
 }
